35f52fbf7770d4fa6466e7605f2a16ea8d73178e
burster11 <55365064+burster11@users.noreply.github.com>
2019-09-22 23:23:44 -0400

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = "";
+        SetCountText();
+        SetlivesText();
+
+    }
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+            SetlivesText();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetlivesText();
+            livesText.text = "Lives: " + lives.ToString();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count == 12)
+        {
+            transform.position = new Vector2(50.0f, 50.0f);
+        }
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives <= 0)
+        {
+            Destroy(gameObject);
+            winText.text = "You Lose!";
+        }
+        if (count >= 21)
+        {
+            winText.text = "You win! Game created by Tyler Trainor!";
+            void SetlivesText()
+            {
+                livesText.text = "Lives: " + lives.ToString();
+                if (lives <= 0)
+                {
+                    Destroy(gameObject);
+                    winText.text = "You Lose!";
+                }
+            }
+        }
+    }
+}
